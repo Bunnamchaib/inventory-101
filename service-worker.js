46f51cb495b9a6eb04988fda6d58c10d31c01cf3
Bunnamchaib <133073786+Bunnamchaib@users.noreply.github.com>
2026-04-22 20:06:13 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const STATIC_CACHE = "chem-static-v2";
+const APP_SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./javascript.html",
+  "./stylesheet.html"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then(function (cache) {
+      return cache.addAll(APP_SHELL_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== STATIC_CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (request.method !== "GET") return;
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      fetch(request).then(function (response) {
+        const responseClone = response.clone();
+        caches.open(STATIC_CACHE).then(function (cache) {
+          cache.put(request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match(request);
+      })
+    );
+  }
+});
